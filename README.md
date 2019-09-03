3a71a8aed87c3adfbeddce6cc007d045fb395957
Kumikurre <atte.jauhiainen@gmail.com>
2019-09-03 15:14:56 +0300

    add sum docs etc

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # iiarsee
-Iiärsee
+Distributed chat system with server-client architecture. 
+
+Url for more docs:
+
+https://drive.google.com/drive/folders/1ZDsQJ17zLuiBYE5VebH6TFDyHK6yb_sk?usp=sharing
---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+
+class socketInterface():
+
+class Channel():
+    users = []
+
+    def user_join():
+        pass
+
+    def _broadcast():
+        pass
+
+    def _receive():
+        pass
+
+    def receive_and_broadcast():
+        _receive()
+        _broadcast()
+
+class User():
+
+class Server():
+    users = []
+    channels = []
+    
+
+if __name__ == '__main__':
