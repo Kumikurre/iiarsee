98a48f1367a8984255fd3c2bfbae932046cb218b
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2019-09-04 16:23:35 +0300

    separate ORM schema and server code

--- server/server.py
+++ server/server.py
@@ -1,33 +1,6 @@
 import socket
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
 
-####### BASIC SQLALCHEMY OBJECT-RELATIONAL-MAPPINGS #######
-Base = declarative_base()
-class Channel(Base):
-    __tablename__ = 'channels'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    users = relationship("User", back_populates="channels")
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
-
-
-class Address(Base):
-    __tablename__ = 'addresses'
-
-    id = Column(Integer, primary_key=True)
-    users = relationship("User", back_populates="addresses")
 
 
 ####### THE PROPRIETARY TCP-INTERFACE FOR COMMUNICATION #######
