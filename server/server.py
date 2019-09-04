b46ebbc329dd2dd927b0766d27b4a3ff5ec46673
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2019-09-04 15:58:59 +0300

    start work on ORM

--- server/server.py
+++ server/server.py
@@ -1,31 +1,69 @@
 import socket
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
+####### BASIC SQLALCHEMY OBJECT-RELATIONAL-MAPPINGS #######
+Base = declarative_base()
+class Channel(Base):
+    __tablename__ = 'channels'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    users = relationship("User", back_populates="channels")
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+
+    id = Column(Integer, primary_key=True)
+    users = relationship("User", back_populates="addresses")
+
+
+####### THE PROPRIETARY TCP-INTERFACE FOR COMMUNICATION #######
 class socketInterface():
+    pass
 
-class Channel():
-    users = []
+# class Channel(Base):
+#     users = []
 
-    def user_join():
-        pass
+#     def user_join():
+#         pass
 
-    def _broadcast():
-        pass 
+#     def _broadcast():
+#         pass 
 
-    def _receive():
-        pass
+#     def _receive():
+#         pass
 
-    def receive_and_broadcast():
-        _receive()
-        _broadcast()
+#     def receive_and_broadcast():
+#         _receive()
+#         _broadcast()
 
-class User():
+# class User(Base):
+#     pass
 
+
+####### THIS IS THE ACTUAL SERVER CLASS #######
 class Server():
     users = []
     channels = []
+    def run(self):
+        while True:
+            pass
 
 
 if __name__ == '__main__':
-    print('Main :-D')
     engine = create_engine('mysql://tshatti:tshattipassu@localhost:3306')
+    server = Server()
+    server.run()
