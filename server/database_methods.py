745a3bc55c4e1a94cc438a637922686bf3e23283
Kumikurre <atte.jauhiainen@gmail.com>
2019-09-17 17:15:32 +0300

    Add some logging defaults

--- server/database_methods.py
+++ server/database_methods.py
@@ -3,11 +3,14 @@ class DBinterface():
         self.engine = engine
 
     def register_user(self):
+        pass
 
     def remove_user(self):
+        pass
 
     def join_channel(self):
+        pass
 
     def leave_channe(self):
-        
+        pass
     
--- server/server.py
+++ server/server.py
@@ -1,49 +1,54 @@
 import socket
 import time
+import logging
+import socket
+import sys
 
 from sqlalchemy import create_engine
 
 import database_init
+import database_methods 
 
 
 ####### THE PROPRIETARY TCP-INTERFACE FOR COMMUNICATION #######
 class socketInterface():
-    pass
+    def __init__(self):
+        pass
 
-# class Channel(Base):
-#     users = []
+    def listen(self):
+        pass
 
-#     def user_join():
-#         pass
-
-#     def _broadcast():
-#         pass 
-
-#     def _receive():
-#         pass
-
-#     def receive_and_broadcast():
-#         _receive()
-#         _broadcast()
-
-# class User(Base):
-#     pass
-
+    def send_channel(self):
+        pass
 
 ####### THIS IS THE ACTUAL SERVER CLASS #######
 class Server():
-    users = []
-    channels = []
     
-    def __init__(self):
-        print('Initializing database')
+    def __init__(self, logger):
+        logger = logger
+        logger.info('Initializing database')
         engine = create_engine('mysql://tshatti:tshattipassu@db:3306')
-        database_init.initialize(engine)
+        database_init.initialize(engine, logger)
+        logger.info('Database initialized')
+        # DBiface = database_methods(engine)
 
     def run(self):
-        print('Server started')
-        
+        logger.info('Server started')
+        totaltime = 0
+        while True:
+            time.sleep(5)
+            totaltime += 5
+            logger.info(f'waited total of {totaltime} seconds')
+
 if __name__ == '__main__':
-    print('Starting server...')
-    server = Server()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    logger.info('Starting server...')
+    server = Server(logger)
     server.run()
