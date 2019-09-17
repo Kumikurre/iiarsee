5ea186239e5591fad78bddbb12dec3c214444b43
Kumikurre <atte.jauhiainen@gmail.com>
2019-09-17 12:29:04 +0300

    add skeleton for db manipulations

---
+++ server/database_methods.py
@@ -0,0 +1,13 @@
+class DBinterface():
+    def __init__(self, engine):
+        self.engine = engine
+
+    def register_user(self):
+
+    def remove_user(self):
+
+    def join_channel(self):
+
+    def leave_channe(self):
+        
+    
