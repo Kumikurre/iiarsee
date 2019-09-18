60fc921084c30c4a6bcce9a289f107543d16c48e
JanneEskola <jaeskola@student.oulu.fi>
2019-09-18 15:26:32 +0300

    Add base for registering users

--- server/database_methods.py
+++ server/database_methods.py
@@ -1,9 +1,13 @@
 class DBinterface():
     def __init__(self, engine):
         self.engine = engine
+        conn = engine.connect()
 
-    def register_user(self):
-
+    def register_user(self, name, fullname):
+        self.name = name
+        self.fullname = fullname
+        ins = users.insert().values(name, fullname)
+        conn.execute(ins)
     def remove_user(self):
 
     def join_channel(self):
