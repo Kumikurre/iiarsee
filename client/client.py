44b384250e4015250af081e7ba61a0c759c7b6d5
sakkoja <joellehtela@gmail.com>
2019-10-02 17:36:44 +0300

    Client: ideation for project
    Trying to really understand what we need, how should we approach the
    problems etc..

--- client/client.py
+++ client/client.py
@@ -5,9 +5,20 @@ import logging
 import socket
 import prompt_toolkit
 
-clientId = "test_client"
 
-possible_actions = {"receive": "receive" }
+# should we have a separate worker for p2p messaging to act as a server to the other client, which would take care of the message
+# receiving and notification of the actual client
+
+class ClientSession:
+    # have internal state of the session here
+    # e.g. the channel in which the client is, do we want to have client actively in one channel?
+    # or should we just have the state of each channel displayed to the client at all times with no "active" channel
+    # simply printing 
+
+    # we need a separate process to handle the incoming messages from the server and other clients
+    # that writes the messages to files and notifies the actual interactive client of new messages in a channel
+
+    # should we create the clientS server when we call the login to the actual server.
 
 
 def receive(self, parameter_list):
@@ -16,52 +27,34 @@ def receive(self, parameter_list):
 
 def main():
     session = prompt_toolkit.PromptSession()
-    
+    # do the login right away here (we don't support offline mode :P)
+    # can we pass a socket to another process?
+    # or should we first create the server process here and interact with it for the login, then
+    # move it to the background and start another process for the actual interactive client
 
-    
-        # while True:
-        #     (b"Hey monseaur server. Gimme things. I'm client")
-        #     data = s.recv(1024)
-        #     if not data:
-        #         break
-        #     print(str(data))
-        # switchcases for every possible action
-        # inside a while loop.
-
-        #send the first message connecting to server
-        # while (True):
-        #     #receive the response from server
-        #     #decode the data received
-        #     # based on the action given from server jump to that in the switchcases
-        #     if action == "receive stuff":
-        #         pass
-        #     elif action == "moar":
-        #         pass
-        #     else:
-        #         continue
-
-        # we need to a while loop for the client ui in which the user can send messages, change channels etc etc
-    
-
-        # s.sendall(b"skjfpsjfpokasjpfSPGKPOSKGPODJGPOJ")
-        # while True:
-        #     test=s.recv(1024)
-        #     if not test:
-        #         continue
-        #     print("fuck")
-        #     print(test)
-        # print(test)
+    # TODO Learn how to run separate processes with the asyncio
+    # TODO How to handle the communication/notification between the server process and the client process
+    # TODO Can we separate a common logic for the messages between client and server
+    # TODO Implement actual message/-logic class with actions and re-actions
+    # TODO Implement the command-line-interface for the client
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((args.address, args.port))
 
             try:
+                # here we handle the client giving us a command
+                # interactive shell code here?
+                # have logic to only allow the given commands and retry if not in the possible commands
+                # 
                 text = session.prompt('client> ')
             except KeyboardInterrupt:
                 continue
             except EOFError:
                 break
             else:
+                # here comes there  logic for a given command
+                # do we use a switch case like logic? Have all the possible commands in a dict/function with handles for the corresponding functions as values
+                # 
                 # once the command is entered and no interuption, we enter here
                 print('You entered:', text)
                 print("sending...")
@@ -69,6 +62,8 @@ def main():
                 data = s.recv(1024)
                 if not data:
                     break
+                # here comes the logic after receiving an answer from the server.
+                # another dict/function/class with functions/methods for possible responses?
                 print("Received back:", data.decode())
                 s.close()
     print('Done.')
@@ -99,11 +94,6 @@ if __name__ == '__main__':
     main()
 
 
-
-# TODO: Design the interactive cli.
-# TODO: Design the switchcase and functions for the client logic
-# TODO: Create the separate server to listen for incoming other clients
-# TODO: Glue it all together
 # NOTE: https://opensource.com/article/17/5/4-practical-python-libraries
 # NOTE: https://codeburst.io/building-beautiful-command-line-interfaces-with-python-26c7e1bb54df
 # NOTE: https://realpython.com/python-sockets/
